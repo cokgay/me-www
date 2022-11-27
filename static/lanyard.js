a81c3cc439500cc298a8379e9699bd70ffc96532
cart96 <agolfcart@tuta.io>
2022-11-27 13:00:39 +0300

    switch lanyard from rest api to websocket (#2)

--- static/lanyard.js
+++ static/lanyard.js
@@ -1,8 +1,36 @@
-const LANYARD_REST = 'https://api.lanyard.rest/v1/users';
+const LANYARD_WS = 'wss://api.lanyard.rest/socket';
+const LANYARD_OP = {
+  PRESENCE: 0,
+  HELLO: 1,
+  INITIALIZE: 2,
+  HEARTBEAT: 3,
+};
+const EVENTS_TO_CALLBACK = ['INIT_STATE', 'PRESENCE_UPDATE'];
 
-const initializeLanyard = async (discord_id, callback) => {
-    let result = await fetch(`${LANYARD_REST}/${discord_id}`);
-    let returned = await result.json();
+const initializeLanyard = (discord_id, callback) => {
+  if (discord_id === '') {
+    return;
+  }
 
-    await callback(discord_id, returned.data);
+  let ws = new WebSocket(LANYARD_WS);
+
+  ws.onmessage = ({ data }) => {
+    const received = JSON.parse(data);
+
+    switch (received.op) {
+      case LANYARD_OP.HELLO: {
+        ws.send(JSON.stringify({ op: LANYARD_OP.INITIALIZE, d: { subscribe_to_id: discord_id } }));
+
+        setInterval(() => {
+          ws.send(JSON.stringify({ op: LANYARD_OP.HEARTBEAT }));
+        }, 1000 * 20);
+      }
+
+      case LANYARD_OP.PRESENCE: {
+        if (EVENTS_TO_CALLBACK.includes(received.t)) {
+          callback(discord_id, received.d);
+        }
+      }
+    }
+  };
 };
